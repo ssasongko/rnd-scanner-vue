aa671f46d691b7958c9b805431ee0a86e1396973
Nur Sasongko <kokonur1020@gmail.com>
2023-12-15 09:47:51 +0700

    feat: handling can be open only in mobile

--- src/views/landing/index.vue
+++ src/views/landing/index.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
-    <ScannerQR />
+    <p>Is Mobile ? {{ isOpenFromMobile }}</p>
+    <hr>
+
+    <button
+      class="cursor-not-allowed"
+      :disabled="!isOpenFromMobile"
+      @click="showScanner = !showScanner"
+    >
+      Open Scanner
+    </button>
+    <ScannerQR 
+      v-if="showScanner"
+    />
   </div>
 </template>
 
 <script>
-import ScannerQR from '../../components/ScannerQR.vue';
+  import ScannerQR from '../../components/ScannerQR.vue';
 
   export default {
-    components: { ScannerQR }
-}
+    components: { 
+      ScannerQR 
+    },
+    data() {
+      return {
+        showScanner: false
+      }
+    },
+    computed: {
+      isOpenFromMobile() {
+        //  Check if the user is using a mobile device
+        return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+      },
+    },
+  }
 </script>
 
 <style lang="scss" scoped>
